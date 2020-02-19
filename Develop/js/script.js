b3734d73c4bc2987c624e523920896eaa8c7bc88
Steven Lam <stevenvl@gmail.com>
2020-02-19 02:59:24 -0500

    past,present,future solved

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    var hoursArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+    var hoursArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "12AM", "2AM"];
 
     for (var i = 0; i < hoursArray.length; i++) {
-
-        var rowTime = moment(hoursArray[i],"HH:mmA");
-    
+        
         // console.log(moment(rowTime,"HH:mmA"));
         var newRow = $("<div>");
         newRow.addClass("row", "input-group-prepend");
-        
+        var rowTime = moment(hoursArray[i], "hA").format("hA");
+        console.log(rowTime);
+
         var hourCol = $("<div>");
         hourCol.addClass("col-2", "text-align-center");
-        hourCol.text(hoursArray[i]);
+        hourCol.text(rowTime);
         
         var eventCol = $("<div>");
         eventCol.addClass("col-8");
@@ -20,26 +20,28 @@ $(document).ready(function () {
         eventInput.addClass("form-control");
         
         var saveCol = $("<div>");
-            var saveButton = $("<button>");
-            saveCol.append(saveButton);
-        saveCol.addClass("col-2", "input-group-append");
-        saveCol.text("Save");
+        var saveButton = $("<button>");
+        saveCol.addClass("col-2", "input-group-append", "btn btn-outline-primary");
+        saveButton.text("Save");
         
         newRow.append(hourCol, eventCol, saveCol);
         eventCol.append(eventInput);
+        saveCol.append(saveButton);
         
         $("#timeBlocks").append(newRow);
+        var currentTimeHours = moment().format("hA")
         
-
-        //past = negative \ future = positive
-        // if(moment().diff(rowTime, ) > 0) {
-        // rowTime.addClass("bg-success text-white");
-        // console.log("time check",moment().diff(rowTime));
-        // } else if(moment().diff(rowTime, ) < 0){
-        // rowTime.addClass("bg-danger text-white");
-        // }
+        if(currentTimeHours === rowTime) {
+        newRow.addClass("bg-dark text-white");
+        }
+        if(moment().isBefore(moment(hoursArray[i], "hA"))) {
+        newRow.addClass("bg-success text-white");
+        console.log("time check",moment().diff(rowTime));
+        } else if(moment().isAfter(moment(hoursArray[i], "hA" ))) {
+        newRow.addClass("bg-danger text-white");
+        }
+        
     }
-    
     var currentTime = $("<time>");
     currentTime.text(moment().format("dddd, MMMM Do YYYY  h:mmA"));
     $("#currentDay").append(currentTime);
